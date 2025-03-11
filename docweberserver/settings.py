b83c7cb778bb491915b5ff69c0a48850156b87ed
laurence <lfish@visibleaccess.net>
2025-03-11 16:04:06 -0700

    Remove logging to file

--- docweberserver/settings.py
+++ docweberserver/settings.py
@@ -155,12 +155,12 @@ LOGGING = {
     },
 
     'handlers': {
-        'file': {
-            'level': 'DEBUG',
-            'class': 'logging.FileHandler',
-            'filename': os.path.join(os.getcwd(), "system.log"),
-            'formatter': 'simple'
-        },
+        #'file': {
+        #    'level': 'DEBUG',
+        #    'class': 'logging.FileHandler',
+        #   'filename': os.path.join(os.getcwd(), "system.log"),
+         #   'formatter': 'simple'
+        #},
         'console': {
             'level': 'DEBUG',
             'class': 'logging.StreamHandler',
