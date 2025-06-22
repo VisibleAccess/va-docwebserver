c71331a5af074ca43337cc5c47c3c441dab34489
laurence <lfish@visibleaccess.net>
2025-06-22 16:28:38 -0700

    Only write wireguard file if there's data

--- main/models.py
+++ main/models.py
@@ -14,8 +14,9 @@ def parse_register_response(response):
     try:
         for item in response['cmd']:
             if item['type'] == "wireguard":
-                with open("/etc/wireguard/wg.xx", 'w') as f:
-                    f.write(item['data'])
+                if len(item['data']):
+                    with open("/etc/wireguard/wg.xx", 'w') as f:
+                        f.write(item['data'])
 
     except Exception as e:
         print(e)
